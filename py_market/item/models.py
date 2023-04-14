a2ef3abf535ce73407b35cdab627aad09d7f776f
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-04-14 00:25:23 -0500

    added the item model to models.py and ensured it was visible in the control panel by registering it in admin.py

--- py_market/item/models.py
+++ py_market/item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,5 +11,18 @@ class Category(models.Model):
         verbose_name_plural ="Categories"
     
     #this ensures the item names show up properly in the admin page
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
