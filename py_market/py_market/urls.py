06c81aeb280926efbae89193f61afb0195cc6e65
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-04-15 00:42:49 -0500

    finished up getting the item and category visible pn the front page, including images for the items, started work on the item detailed view via detail.html and the relevant edits to views and urls as well as creating a new template folder and details.html for the items app

--- py_market/py_market/urls.py
+++ py_market/py_market/urls.py
@@ -18,12 +18,17 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import index, contact
+#do not do this in prod
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',index, name='index'),
     path('contact/', contact, name='contact'),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#similarly do not do this in prod
+
+
 
 
 
