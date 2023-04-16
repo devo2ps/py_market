63f4bd471305ca7ac2e855fd1050d2587ddb306d
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-04-16 01:07:20 -0500

    just made sure related items are included at bottom, which work for some reason while the main detail page doesnt for some unknown reason

--- py_market/item/views.py
+++ py_market/item/views.py
@@ -6,7 +6,7 @@ from .models import Item
 
 def detail(request,pk):
     item=get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:1]
 
     return render(request, 'item/detail.html', {
         'item':item.pk,
