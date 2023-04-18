7e3839fff9ae98c43b7c21fb57024485cd45ea5e
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-04-17 23:01:13 -0500

    mostly finished up the browse functionality but still need to figure out the formatting/graying out for categories in the sidebar

--- py_market/item/views.py
+++ py_market/item/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def items(request):
+    query=request.GET.get('query','')
+    category_id=request.GET.get('category', 0)
+    items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query) )
+
+    return render(request, 'item/items.html', {
+        'items':items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+
+    })
+
 
 def detail(request, pk):
     item=get_object_or_404(Item, pk=pk)
@@ -12,7 +30,7 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item':item, 
         'related_items': related_items})
-   # })
+
 
 @login_required
 def new(request):
