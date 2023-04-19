99840669965af632922ed53ff24c1181ef087876
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-04-18 21:22:19 -0500

    had to mess around with the query statements a bit but got the category selection feature sorted out as well, onto user communications

--- py_market/item/views.py
+++ py_market/item/views.py
@@ -11,8 +11,13 @@ def items(request):
     category_id=request.GET.get('category', 0)
     items = Item.objects.filter(is_sold=False)
     categories = Category.objects.all()
+
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query) )
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+    
 
     return render(request, 'item/items.html', {
         'items':items,
@@ -21,6 +26,8 @@ def items(request):
         'category_id': int(category_id),
 
     })
+
+    
 
 
 def detail(request, pk):
